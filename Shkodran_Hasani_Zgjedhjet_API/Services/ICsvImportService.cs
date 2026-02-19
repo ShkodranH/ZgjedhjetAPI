34bd908465ed3309b675f5231ef3a6e660cece8e
Shkodran Hasani <137167916+ShkodranH@users.noreply.github.com>
2026-02-19 18:55:25 +0100

    Delete Shkodran_Hasani_Zgjedhjet_API directory

--- Shkodran_Hasani_Zgjedhjet_API/Services/ICsvImportService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shkodran_Hasani_Zgjedhjet_API.Models.DTOs;
-
-namespace Shkodran_Hasani_Zgjedhjet_API.Services
-{
-    public interface ICsvImportService
-    {
-        Task<CsvImportResponse> ImportCsvAsync(IFormFile file);
-    }
-}
